4430acaa341627fa5a66f2f62a0c8eb326e98030
MshariSq <misharysq@gmail.com>
2023-01-23 23:16:50 +0300

    bug with moving header in Administrator with dashboard

--- Administrator.php
+++ Administrator.php
@@ -277,24 +277,6 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
             </div>
         </div>
     </div>
-    <script>
-        window.onscroll = function() {
-            myFunction()
-        };
-
-        var header = document.getElementById("myHeader");
-        var sticky = header.offsetTop;
-
-        function myFunction() {
-            if (window.pageYOffset > sticky) {
-                header.classList.add("sticky");
-            } else {
-                header.classList.remove("sticky");
-            }
-        }
-    </script>
-
-
 </body>
 
 </html>
